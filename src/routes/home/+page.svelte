d2d480c7aa4a21c36017c59cc937766990c4f233
ChrisMadeda-Dev <chrismadeda.dev@gmail.com>
2024-05-17 11:42:51 +0300

    first commit

---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+    function checkDetails(){
+        const a = a
+    }
+</script>
+
+
+<div class="h-[100vh] w-[100vw] bg-white flex flex-col justify-center items-center">
+	<div
+		class="h-[50vh] w-[50%] flex flex-col justify-center items-center rounded-lg border border-gray-300 bg-white gap-3"
+	>
+		<input
+			type="text"
+			placeholder="Enter PLei code"
+			class="input input-bordered w-[50%] h-[70px] bg-gray-50"
+		/>
+		<input
+			type="text"
+			placeholder="Enter password"
+			class="input input-bordered w-[50%] h-[70px] bg-gray-50"
+		/>
+		<button class="btn btn-primary w-[50%]">Primary</button>
+	</div>
+</div>
